fb75c5967fe50a94122e98744743d28151df4826
JuanEstebanP04 <90432563+JuanEstebanP04@users.noreply.github.com>
2024-04-03 08:52:35 -0500

    Programa realizado en las clase de Programación en Bash Shell

---
+++ 24_operacionesArchivo.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+# Programa para ejemplificar las operaciones de un archivo
+
+echo "Operaciones de un archivo"
+mkdir -m 755 backupScripts
+echo -e "\nCopiar los scripts del directorio actual al nuevo directorio BackupScripts"
+cp *.* backupScripts/
+ls -la backupScripts/
+
+echo -e"\nMover el directorio de backupScripts a otra ubicación: $HOME"
+mv backupScripts $HOME
+
+echo -e"\nEliminar los archivos .txt"
+rm *.txt
